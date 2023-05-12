68631d9520ddc19ee85cabba0fba85010d333941
unknown <savif4991@gmail.com>
2023-05-13 02:28:09 +0900

    폴더별로 데이터, 퀀트투자 레퍼런스는 따로 보관해놓았고 2022년도 재무상태표와 손익계산서 내부저장 기능 점검 완료했습니다. 승연님이 알려주신 DART공시 사이트에서 엑셀 템플릿 파일에서 불러온 뒤 csv파일로 추출했으며, 이를 csv모듈 활용해 딕셔너리 리스트로 저장했습니다. 퀀트투자 교재에서의 형식과 완전 동일한 것으로 보이네요. 다만 특정 문자열이 첫 키인 재무제표종류 키 값에 딸려붙어오는 문제가 발생해 하드코딩으로 그 부분만 일단 막아놓았습니다. 다른 문제는 겪지 못했습니다.

---
+++ daejin/get_BS_PL.py
@@ -0,0 +1,31 @@
+import csv
+
+def get_BS():
+	f = open('./data/별도재무상태표_2022.csv', 'rt', encoding='UTF8')
+	BS = []
+	reader = csv.DictReader(f)
+	#재무제표종류 키 값이 \ufeff가 앞에 붙어 오염되는 것을 확인하여 pop으로 제거 후 다시 추가하였습니다.
+	for row in reader:
+		row['재무제표종류'] = row.pop('\ufeff재무제표종류')
+		BS.append(row)
+	f.close()
+	return BS
+
+def get_PL():
+	f = open('./data/손익계산서_2022.csv', 'rt', encoding='UTF8')
+	PL = []
+	reader = csv.DictReader(f)
+	#재무제표종류 키 값이 \ufeff가 앞에 붙어 오염되는 것을 확인하여 pop으로 제거 후 다시 추가하였습니다.
+	for row in reader:
+		row['재무제표종류'] = row.pop('\ufeff재무제표종류')
+		PL.append(row)
+	f.close()
+	return PL
+
+#재무상태표와 손익계산서 데이터를 가져왔습니다. 저장 형식은 딕셔너리 리스트입니다.
+BS = get_BS()
+for i in range(10):
+	print(BS[i])
+PL = get_PL()
+for i in range(10):
+	print(PL[i])
