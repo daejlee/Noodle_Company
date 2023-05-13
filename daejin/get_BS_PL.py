ff41501896c2bfca5f2c8da08f17e59396e92aad
unknown <savif4991@gmail.com>
2023-05-13 19:28:04 +0900

    파일경로 수정

--- daejin/get_BS_PL.py
+++ daejin/get_BS_PL.py
@@ -1,7 +1,7 @@
 import csv
 
 def get_BS():
-	f = open('./data/별도재무상태표_2022.csv', 'rt', encoding='UTF8')
+	f = open('./data/2022/별도재무상태표_2022.csv', 'rt', encoding='UTF8')
 	BS = []
 	reader = csv.DictReader(f)
 	#재무제표종류 키 값이 \ufeff가 앞에 붙어 오염되는 것을 확인하여 pop으로 제거 후 다시 추가하였습니다.
@@ -12,7 +12,7 @@ def get_BS():
 	return BS
 
 def get_PL():
-	f = open('./data/손익계산서_2022.csv', 'rt', encoding='UTF8')
+	f = open('./data/2022/손익계산서_2022.csv', 'rt', encoding='UTF8')
 	PL = []
 	reader = csv.DictReader(f)
 	#재무제표종류 키 값이 \ufeff가 앞에 붙어 오염되는 것을 확인하여 pop으로 제거 후 다시 추가하였습니다.
@@ -30,4 +30,4 @@ PL = get_PL()
 for i in range(10):
 	print(PL[i])
 
-#살펴본 결과 모든 value가 str타입이네요.. 종목코드나 업종, 전기, 전전기 등은 숫자 자료형으로 바꿔주는 게 나을 것 같네욥
+#살펴본 결과 모든 value가 str타입이네요.. 종목코드나 업종, 전기, 전전기 등은 숫자 자료형으로 바꿔주는 게 나을 것 같아욥.
