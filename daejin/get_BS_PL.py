3261c95435391184a39bdaca5273d222c4aaadfe
unknown <savif4991@gmail.com>
2023-05-16 22:15:33 +0900

    BS, PL 한 기업에 대해 묶었고, 모든 년도 데이터 포함시켰습니다.

--- daejin/get_BS_PL.py
+++ daejin/get_BS_PL.py
@@ -2,58 +2,87 @@ import csv
 
 #keys: '종목코드'		num
 #		'회사명',		str
-#		'시장구분',		str
-#		'업종',			str
-#		'업종명',		str
-# 		'결산월',		num
-# 		'결산기준일',	str
-# 		'보고서종류',	str
-# 		'통화',			str
-# 		'항목코드',		str
+#		'시장구분',		str -> 삭제
+#		'업종',			str -> 삭제
+#		'업종명',		str -> 삭제
+# 		'결산월',		num -> 삭제
+# 		'결산기준일',	str -> 삭제
+# 		'보고서종류',	str -> 삭제
+# 		'통화',			str -> 삭제
+# 		'항목코드',		str -> 삭제
 # 		'항목명',		str
 # 		'당기',			num
 # 		'전기',			num
 # 		'전전기',		num
-# 		'재무제표종류'	str
+# 		'재무제표종류'	str -> 삭제
+# 그리고 기업별로 하나로 묶기.
 
-#재무제표종류 키 값이 \ufeff가 앞에 붙어 오염되는 것을 확인하여 pop으로 제거 후 다시 추가하였고, 종목코드의 []를 제거하였고, 단위를 통일하기 위해 ,를 제거하였습니다.
+#필요없는 키들을 삭제했고, 종목코드의 []를 제거하였고, 단위를 통일하기 위해 당기, 전기, 전전기의 ,를 제거하였습니다.
 def cleanse_data(row):
-	row['재무제표종류'] = row.pop('\ufeff재무제표종류')
+	del(row['\ufeff재무제표종류'])
+	del(row['시장구분'])
+	del(row['업종'])
+	del(row['업종명'])
+	del(row['결산월'])
+	del(row['결산기준일'])
+	del(row['보고서종류'])
+	del(row['통화'])
+	del(row['항목코드'])
 	row['종목코드'] = row['종목코드'].replace('[', '').replace(']', '')
 	row['당기'] = row['당기'].replace(',', '')
 	row['전기'] = row['전기'].replace(',', '')
 	row['전전기'] = row['전전기'].replace(',', '')
+	num_val_lst = ['종목코드', '당기', '전기', '전전기']
+	for item in row:
+		if item in num_val_lst and row[item] != '':
+			row[item] = int(row[item])
+	return row
 
+#BS는 회사명을 키로 하는 이중 딕셔너리입니다. BS[회사명][항목명] = 값
 def get_BS():
-	num_val_lst = ['종목코드', '결산월', '당기', '전기', '전전기']
-	f = open('./data/2022/별도재무상태표_2022.csv', 'rt', encoding='UTF8')
 	BS = []
-	reader = csv.DictReader(f)
-	for row in reader:
-		cleanse_data(row)
-		for item in row:
-			if item in num_val_lst and row[item] != '':
-				row[item] = int(row[item])
-		BS.append(row)
-		print(row)
-	f.close()
+	for i in range (7):
+		file_name = "./cache/inputyear/inputyear_BS.csv".replace('inputyear', str(2016 + i))
+		f = open(file_name, 'rt', encoding='UTF8')
+		reader = csv.DictReader(f)
+		cleansed_data = []
+		for row in reader:
+			cleansed_data.append(cleanse_data(row))
+		globals()['BS_{}'.format(i)] = {}
+		prev_row = cleansed_data[0]
+		lst = []
+		for row in cleansed_data:
+			if prev_row['종목코드'] != row['종목코드']:
+				globals()['BS_{}'.format(i)][prev_row['종목코드']] = lst
+				lst = []
+			prev_row = row
+			lst.append(row)
+		f.close()
+		BS.append(globals()['BS_{}'.format(i)])
 	return BS
 
 def get_PL():
-	f = open('./data/2022/손익계산서_2022.csv', 'rt', encoding='UTF8')
 	PL = []
-	reader = csv.DictReader(f)
-	for row in reader:
-		cleanse_data(row)
-		PL.append(row)
-	f.close()
+	for i in range (7):
+		file_name = "./cache/inputyear/inputyear_PL.csv".replace('inputyear', str(2016 + i))
+		f = open(file_name, 'rt', encoding='UTF8')
+		reader = csv.DictReader(f)
+		cleansed_data = []
+		for row in reader:
+			cleansed_data.append(cleanse_data(row))
+		globals()['PL_{}'.format(i)] = {}
+		prev_row = cleansed_data[0]
+		lst = []
+		for row in cleansed_data:
+			if prev_row['종목코드'] != row['종목코드']:
+				globals()['PL_{}'.format(i)][prev_row['종목코드']] = lst
+				lst = []
+			prev_row = row
+			lst.append(row)
+		f.close()
+		PL.append(globals()['PL_{}'.format(i)])
 	return PL
 
 #재무상태표와 손익계산서 데이터를 가져왔습니다. 저장 형식은 딕셔너리 리스트입니다.
 BS = get_BS()
-print(BS[0].keys())
-for i in range(10):
-	print(BS[i])
 PL = get_PL()
-for i in range(10):
-	print(PL[i])
