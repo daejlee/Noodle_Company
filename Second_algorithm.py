c985f5dc43efeda0ecb63ca3bfc3d3de411d41f9
BusyGhost <131769189+BusyGhost@users.noreply.github.com>
2023-05-30 13:05:15 +0900

    Add files via upload
    1번 알고리즘 파일에서 first_arg(투자유형 1or2) return { 회사: 점수 점수 점수 점수 종합점수}

---
+++ Second_algorithm.py
@@ -0,0 +1,56 @@
+'''
+1. NCAV / 시가총액 ≥ 150%
+2. 당기순이익 > 0
+3. 부채총계 / 자본총계 ≤ 50%
+4. PER ≤ 10
+'''
+
+from get_BS_PL import *
+from get_sync_data import *
+
+def secarg_point(ncav, cap, debtr, per, a=1.5, b=0.5, c=10, d=300000000000):
+    corp = []
+    ncavr = ncav / cap
+    if ncavr >= a:
+        if debtr <= b:
+            if per <= c and per > 0:
+                if cap >= d:
+                    x, y, z = 5, 2, 3
+                    if ncavr >= 5:
+                        ncavr = 5
+                    if per <= 1:
+                        per = 1
+                    NCAVR = round(100/7 * (ncavr) + 200/7, 2)
+                    DEBTR = round(-100 * debtr + 100, 2)
+                    PER =  round(-50/9 * per + 50/9 + 100, 2)
+                    corp = [NCAVR, DEBTR, PER, round((x*NCAVR+y*DEBTR+z*PER)/(x+y+z), 2)]
+                    return corp				
+    return
+
+def sec_arg(a=1.5, b=0.5, c=10, d=300000000000):
+	secarg_corp = {}
+	OUTLAW_LIST = []
+	for key in MERGED_REPORT:
+		company = key
+		
+		CAP = get_market_cap(company)
+		
+		if CAP == NOT_EXISTING:		#시가총액이 네이버 증권에 존재하지 않는 기업은 패스
+			continue
+		CAP = float(CAP.replace(',', '')) * 100000000
+	
+		try:
+			ncav = get_dangi(company, "유동자산") - get_dangi(company, "부채총계")
+			NET_INCOME = get_dangi(company, "당기순이익")
+			per = CAP / NET_INCOME
+			debtr = get_dangi(company, "부채총계") / get_dangi(company, "자본총계")
+		except:
+			OUTLAW_LIST.append(company)		#오류가 발생하는 기업들은 따로
+			continue
+
+		corp = []
+		corp = secarg_point(ncav, CAP, debtr, per, a, b, c, d)
+		if corp != None: secarg_corp[company] = corp
+	return secarg_corp
+
+print(sec_arg())
